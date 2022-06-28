a0b03812834ec77e800637ee3fc51eebb00d4b23
Lineisha <lineishagibson@gmail.com>
2022-06-28 10:28:22 -0500

    work

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class StudentTest {
+    @Test
+    public void testCreateStudent(){
+Student name = new Student (1L, "james")
+    }
+    @Test
+    public void  testAllFieldsGetUpdated(){
+
+    }
+    @Test
+    public void testAddingGradesWorks(){
+
+    }
+    @Test
+    public void testAverageGradeWorks(){
+
+    }
+}
